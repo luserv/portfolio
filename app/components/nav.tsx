cccd8ec69c24cef3f25fdc3aa065afe1e191d6d2
user <git.random422@passinbox.com>
2025-07-12 21:16:23 -0500

    new

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 const navItems = {
   '/': {
-    name: 'home',
+    name: 'Home',
   },
   '/projects': {
     name: 'Projects',
--- app/page.tsx
+++ app/page.tsx
@@ -4,21 +4,6 @@ import Image from 'next/image'
 export default function Page() {
   return (
     <section>
-      <picture className='flex flex-row justify-center align-middle mb-8'>
-      <Image
-        src="/images/profile.jpeg"
-        alt="Profile photo"
-        width={150}
-        height={150}
-        className='rounded-full object-cover'
-      />
-      </picture>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter text-center">
-        Luser
-      </h1>
-      <p className="mb-4">
-        {`Soy un usuario entusiasta de Debian linux ya que al usarlo con xcfe4 no llega a usar mas de un giga`}
-      </p>
       <div className="my-8">
         <BlogPosts />
       </div>
